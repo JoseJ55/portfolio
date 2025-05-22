ec72d234944af6e34631c453f2ea92afed7d7b47
JoseJ55 <josejimenez826@gmail.com>
2025-05-22 14:53:49 -0700

    added scroll animation

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,7 +1,46 @@
+import { animate, motion, useMotionValue, useScroll } from 'motion/react';
+import { useEffect, useRef } from 'react';
+
 import { Header, Navbar, Projects } from '../../components';
 
 
 export const Home = () => {
+    const containerRef = useRef<HTMLDivElement | null>(null);
+
+    const { scrollXProgress } = useScroll({ container: containerRef });
+
+    const scrollX = useMotionValue(0);
+
+    useEffect(() => {
+        const container = containerRef.current;
+
+        if (!container) return;
+
+        let currentScroll = container.scrollLeft;
+
+        const handleWheel = (e: WheelEvent) => {
+            e.preventDefault();
+            const delta = e.deltaY * 1.6;
+            currentScroll = container.scrollLeft + delta;
+
+            scrollX.set(container.scrollLeft);
+
+            animate(scrollX, currentScroll, {
+                duration: 0.1,
+                ease: 'easeOut',
+                onUpdate: (latest) => {
+                    container.scrollLeft = latest;
+                },
+            });
+        };
+
+        container.addEventListener('wheel', handleWheel, { passive: false });
+
+        return () => {
+            container.removeEventListener('wheel', handleWheel);
+        };
+    }, [scrollX]);
+
     return (
         <div className='w-screen h-screen bg-black relative'>
             <Navbar>
@@ -34,10 +73,28 @@ export const Home = () => {
                     </div>
                 </div>
 
-                <div className='relative w-full h-full z-[1] flex overflow-auto overflow-y-hidden'>
+                <motion.div
+                    ref={containerRef}
+                    className={
+                        `
+                            relative w-full h-full z-[1] flex overflow-y-hidden transition-all duration-100
+                            [&::-webkit-scrollbar]:hidden [-ms-overflow-style:none] [scrollbar-width:none]
+                        `
+                    }
+                >
                     <Header />
                     <Projects />
-                </div>
+                </motion.div>
+
+                <motion.div
+                    style={{ scaleX: scrollXProgress }}
+                    className={
+                        `
+                            fixed bottom-8 left-0 w-full h-[10px] bg-accent origin-center z-40 transition-all 
+                            duration-100
+                        `
+                    }
+                />
             </Navbar>
         </div>
     );
